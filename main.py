cc22c1d24f3071ab7afedc80fa703be499b02329
joydeepC-code <joydeepchanda1986@gmail.com>
2022-06-08 16:54:31 +0530

    new features

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import random
-x = random.randint(0,10)
+x = random.randint(1,10)
 
 for t in range(0, 5):
-    y = int(input("Guess the number, you have 5 chances: "))
+    y = int(input("Guess the number, you have 5 chances, the number is between 1 - 10: "))
 
     if x == y:
         print("Correct, you have guessed the number in", t + 1, "chances")
         break
+
+    if y > 10:
+        print("Incorrect, the number is between 1 - 10")
 
     elif y > x:
         print("Your number is too high")
 
     elif x > y:
         print("Your number is too low")
+
+if t == 4 and x != y:
+    print("You couldn't guess the number, it was", x)
